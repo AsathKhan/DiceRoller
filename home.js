11199e22b74692327456a18d5cd60d5604cd05b4
AsathKhan <asath2k2@gmail.com>
2024-10-06 14:20:44 +0530

    testing captchaGeneration

--- home.js
+++ home.js
@@ -13,3 +13,30 @@ function rollDice(){
     diceImages.innerHTML=images.join('');
 }
 
+//authentic.html
+function generateCaptcha(){
+
+const captchaLength = 8;
+const incNumbers = true, incLowerAlphabets = true, incUpperAlphabets = true, incSymbols = true;
+
+const numbers = '0123456789';
+const lowerAlphabets = 'abcdefghijklmnopqrstuvwxyz';
+const upperAlphabets = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const symbols = '!@#%&';
+
+let allowedChar = '';
+
+allowedChar += incNumbers ? numbers : '';
+allowedChar += incLowerAlphabets ? lowerAlphabets : '';
+allowedChar += incUpperAlphabets ? upperAlphabets : '';
+allowedChar += incSymbols ? symbols : '';
+console.log(allowedChar);
+
+for(i=0; i<=captchaLength; i++){
+    randomIndex = Math.floor(Math.random()*allowedChar.length);
+    console.log(randomIndex);
+}
+
+}
+
+generateCaptcha();
