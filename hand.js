037e3f66d5a2865324a55677cec7a47d891ad49c
AsathKhan <asath2k2@gmail.com>
2024-10-22 13:15:32 +0530

    game working fine

---
+++ hand.js
@@ -0,0 +1,31 @@
+const choices = ['rock', 'paper', 'scissor'];
+const playerDisplay = document.getElementById('playerDisplay');
+const computerDisplay = document.getElementById('computerDisplay');
+const resultDisplay = document.getElementById('resultDisplay');
+
+function playGame(playerChoice){
+    const computerChoice = choices[Math.floor(Math.random()*3)];
+    let result = "";
+
+    if (playerChoice === computerChoice){
+        result = "IT's A TIE!"
+    }else{
+        switch(playerChoice){
+            case "rock":
+                result = (computerChoice === "scissor") ? "YOU WIN!" : "YOU LOSE!";
+                break;
+            case "paper":
+                result = (computerChoice === "rock") ? "YOU WIN!" : "YOU LOSE!";
+                break;
+
+            case "scissor":
+                result = (computerChoice === "paper") ? "YOU WIN!" : "YOU LOSE!";
+                break;
+        }
+    }
+
+    playerDisplay.textContent = `PLAYER: ${playerChoice}`;
+    computerDisplay.textContent = `COMPUTER: ${computerChoice}`;
+    resultDisplay.textContent = `${result}`;
+
+}
