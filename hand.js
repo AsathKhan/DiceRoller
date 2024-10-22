d8af21c726bf7b0de622128a2da594146ac063a8
AsathKhan <asath2k2@gmail.com>
2024-10-22 13:48:32 +0530

    hand completed

--- hand.js
+++ hand.js
@@ -2,6 +2,11 @@ const choices = ['rock', 'paper', 'scissor'];
 const playerDisplay = document.getElementById('playerDisplay');
 const computerDisplay = document.getElementById('computerDisplay');
 const resultDisplay = document.getElementById('resultDisplay');
+const playerScoreDisplay = document.getElementById('playerScoreDisplay');
+const computerScoreDisplay = document.getElementById('computerScoreDisplay');
+
+let playerScore = 0;
+let computerScore = 0;
 
 function playGame(playerChoice){
     const computerChoice = choices[Math.floor(Math.random()*3)];
@@ -28,4 +33,22 @@ function playGame(playerChoice){
     computerDisplay.textContent = `COMPUTER: ${computerChoice}`;
     resultDisplay.textContent = `${result}`;
 
+    resultDisplay.classList.remove('greenText', 'redText');
+
+    switch(result){
+        case "YOU WIN!":
+            resultDisplay.classList.add("greenText");
+            playerScore++;
+            playerScoreDisplay.textContent = playerScore;
+            break;
+
+        case "YOU LOSE!":
+            resultDisplay.classList.add("redText");
+            computerScore++;
+            computerScoreDisplay.textContent = playerScore;
+            break;
+    }
 }
+
+
+
